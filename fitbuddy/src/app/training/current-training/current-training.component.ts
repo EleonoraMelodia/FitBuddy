37e356be87f3c9db20ae49e4a2df7681e4692b87
Eleonora Melodia <eleonoramelodia92@gmail.com>
2024-02-23 13:56:35 +0100

    UI: current training updated with current progress

--- fitbuddy/src/app/training/current-training/current-training.component.ts
+++ fitbuddy/src/app/training/current-training/current-training.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { MatDialog } from '@angular/material/dialog';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { StopTrainingComponent } from "./stop-training.component";
 @Component({
   selector: "app-current-training",
@@ -7,10 +7,15 @@ import { StopTrainingComponent } from "./stop-training.component";
   styleUrl: "./current-training.component.css",
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter<void>();
   progress = 0;
   timer: any;
-constructor( private dialog : MatDialog){}
+
+  constructor(private dialog: MatDialog) {}
   ngOnInit(): void {
+    this.startOrResumeTimer();
+  }
+  startOrResumeTimer() {
     this.timer = setInterval(() => {
       this.progress += 5;
       if (this.progress >= 100) {
@@ -19,8 +24,19 @@ constructor( private dialog : MatDialog){}
     }, 1000);
   }
 
-      onStop(){
-        clearInterval(this.timer);
-        this.dialog.open(StopTrainingComponent)
-    }
+  onStop() {
+    clearInterval(this.timer);
+    const dialog = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress,
+      },
+    });
+
+    dialog.afterClosed().subscribe((result) => {
+      if (result) {
+        this.trainingExit.emit();
+      }
+      else {this.startOrResumeTimer()}
+    });
+  }
 }
